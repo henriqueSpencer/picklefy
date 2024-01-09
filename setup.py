82644485cf86b1bdaf856b7a4334e7b7c2f8415a
henriqueSpencer <henriquespencer11@gmail.com>
2024-01-08 22:51:45 -0300

    init2

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
+import pathlib
 from setuptools import setup, find_packages
-import codecs
-import os
+
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Um facilitador para trabalhar com pickle files'
-LONG_DESCRIPTION = 'sera que vai dar certo?'
 # para criar o packege
 #python3 setup.py sdist bdist_wheel
 # dist é o q fazemos o upload
-pip isntall twine
+# pip isntall twine
 # twine upload dist/*
 
 # Setting up
@@ -18,10 +17,10 @@ setup(
     author="Henrique Spencer)",
     author_email="<henriquespencer11@gmail.com>",
     description=DESCRIPTION,
+    long_description=pathlib.path("README.md").read_text(),
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    install_requires=['pickle'],
     keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
     classifiers=[
         "Development Status :: 1 - Planning",
@@ -30,5 +29,7 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.6',
 )
